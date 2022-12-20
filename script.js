2a7518283afadf11ce1b06e2cb4a29f44d70f8b3
nikita rawat <nikitarawat7072@gmail.com>
2022-12-20 11:49:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// navigation
+
+
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+       nav.classList.add("scroll-on"); 
+    }else{
+        nav.classList.remove("scroll-on"); 
+    }
+}
+
+document.addEventListener("DOMContentLoaded",() => {
+    function counter(id , start , end , duration){
+        let obj = document.getElementById(id),
+        current = start ,
+        range = end - start ,
+        increment = end > start ? 1 :-1 ,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if(current == end){
+                clearInterval(timer);
+            }
+        }, step)
+
+        
+
+    }
+    counter("count1" , 0 , 1234 , 3000);
+    counter("count2" , 100 , 2000 , 2000);
+    counter("count3" , 0 , 3459 , 2500);
+    counter("count4" , 0 , 1834 ,7989)
+
+});
+
+
+
+
+
+
